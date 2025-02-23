1538b74d1e259b91a069b2c442976b8bf92f63a5
glabrie <glabrie85@gmail.com>
2025-02-23 09:09:37 -0500

    Initial commit on the new branch

---
+++ hosts/aurora/configuration.nix
@@ -0,0 +1,124 @@
+# NixOS config with flakes and home-manager. Very messy.
+
+{ config, pkgs, inputs, ... }:
+
+{
+  imports =
+    [
+      ./hardware-configuration.nix
+      inputs.home-manager.nixosModules.default
+  ];
+
+  # Support for Flakes and commands, Unfree
+  nix.settings.experimental-features = ["nix-command" "flakes"];
+  nixpkgs.config.allowUnfree = true;
+
+  # Bootloader
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  services.gvfs.enable = true;
+  services.udisks2.enable = true;
+
+  # Power Management for battery life
+  services.power-profiles-daemon.enable = false;
+  services.thermald.enable = true;
+  services.tlp = {
+    enable = true;
+    settings = {
+      CPU_SCALING_GOVERNOR_ON_AC = "performance";
+      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+      CPU_MIN_PERF_ON_AC = 0; 
+      CPU_MAX_PERF_ON_AC = 100;
+      CPU_MIN_PERF_ON_BAT = 0;
+      CPU_MAX_PERF_ON_BAT = 20;
+
+    };
+  };
+
+  # Network
+  networking = {
+    hostName = "aurora";
+    networkmanager.enable = true;
+  };
+
+  # Timezone and locale
+  time.timeZone = "America/Toronto";
+  i18n.defaultLocale = "en_CA.UTF-8";
+
+  # Xserver and graphics
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+
+  # Enable the GNOME Desktop Environment.
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
+
+# Keymap
+  services.xserver.xkb = {
+    layout = "us,ca";
+    variant = "fr";
+    options = "grp:win_space_toggle";
+  };
+
+  # Sound - Pipewire
+  services.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+};
+
+  # NixOS Options
+  programs.zsh.enable = true;
+  services.flatpak.enable = true;
+
+  # Userland
+  users.users.ghil = {
+    isNormalUser = true;
+    description = "ghil";
+    extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
+    packages = with pkgs; [
+    ];
+  };
+
+  #Home manager
+  home-manager = {
+    extraSpecialArgs = { inherit inputs; };
+    users = {
+      "ghil" = import ./home.nix;
+    };
+  };
+
+  # Packages installed system wide
+  environment.systemPackages = with pkgs; [
+    acpi
+    appimage-run
+    distrobox
+    fastfetch
+    git
+    gcc
+    kitty
+    lazygit
+    libgcc
+    neovim
+    ripgrep
+    wl-clipboard
+    wget
+];
+
+ # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
---
+++ hosts/aurora/home.nix
@@ -0,0 +1,83 @@
+# nixOS home-manager config
+{ config, pkgs, ... }:
+
+{
+  home.username = "ghil";
+  home.homeDirectory = "/home/ghil";
+  home.stateVersion = "24.11"; # do not change please.
+ 
+  nixpkgs.config.allowUnfree = true;
+  home.packages = with pkgs; [
+    # Apps
+    atuin
+    bottom
+    cava
+    cliphist
+    discord
+    dmenu
+    dunst
+    element-desktop
+    fractal
+    floorp
+    gh
+    google-chrome
+    gnome-tweaks
+    grimblast
+    hyprpaper
+    ncspot
+    obsidian
+    ranger
+    rofi-wayland
+    slack
+    tana
+    telegram-desktop
+    vscode-with-extensions
+    waybar
+    w3m
+    wpsoffice
+
+    # Ricing
+    catppuccin
+    catppuccin-gtk
+    
+    # Gnome Extensions
+    gnomeExtensions.appindicator
+    gnomeExtensions.blur-my-shell
+    gnomeExtensions.dash-to-dock
+
+
+ ];
+
+  home.file = {
+
+  };
+
+  home.sessionVariables = {
+     EDITOR = "nvim";
+  };
+
+  programs.home-manager.enable = true;
+
+  programs.starship = {
+    enable = true;
+    enableZshIntegration = true;
+    settings = {
+    };
+  };
+  programs.zsh = {
+    enable = true;
+    shellAliases = {
+      boot-garbage = "sudo /run/current-system/bin/switch-to-configuration boot";
+      collect-garbage = "nix-collect-garbage -d";
+      dots ="cd ~/.dotfiles/";
+      flup = "nix flake update";
+      ga = "git add *";
+      gc = "git commit --verbose";
+      gp = "git push";
+      ll = "ls -l";
+      v = "nvim";
+      update = "sudo nixos-rebuild switch --flake ~/.dotfiles/#default";
+    };
+};
+
+}
