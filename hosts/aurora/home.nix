6f0e377bd93922e8ea5132abe1f025620acdc243
glabrie <glabrie85@gmail.com>
2025-02-27 21:04:38 -0500

    update and steam

--- hosts/aurora/home.nix
+++ hosts/aurora/home.nix
@@ -48,6 +48,13 @@
 
   programs.home-manager.enable = true;
 
+  programs.steam = {
+  enable = true;
+  remotePlay.openFirewall = true; # Open ports in the firewall for Steam Remote Play
+  dedicatedServer.openFirewall = true; # Open ports in the firewall for Source Dedicated Server
+  localNetworkGameTransfers.openFirewall = true; # Open ports in the firewall for Steam Local Network Game Transfers
+};
+
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
