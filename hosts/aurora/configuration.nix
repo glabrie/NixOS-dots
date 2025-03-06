2779a619910b808a5ecf5d628c6bf91861ef10b4
glabrie <glabrie85@gmail.com>
2025-03-06 11:40:24 -0500

    added nvidia

--- hosts/aurora/configuration.nix
+++ hosts/aurora/configuration.nix
@@ -6,6 +6,7 @@
   imports =
     [
       ./hardware-configuration.nix
+      ./graphics.nix
       inputs.home-manager.nixosModules.default
   ];
 
@@ -19,23 +20,23 @@
   services.gvfs.enable = true;
   services.udisks2.enable = true;
 
-  # Power Management for battery life
-  services.power-profiles-daemon.enable = false;
-  services.thermald.enable = true;
-  services.tlp = {
-    enable = true;
-    settings = {
-      CPU_SCALING_GOVERNOR_ON_AC = "performance";
-      CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
-      CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
-      CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-      CPU_MIN_PERF_ON_AC = 0; 
-      CPU_MAX_PERF_ON_AC = 100;
-      CPU_MIN_PERF_ON_BAT = 0;
-      CPU_MAX_PERF_ON_BAT = 20;
-
-    };
-  };
+ # # Power Management for battery life
+ # services.power-profiles-daemon.enable = false;
+ # services.thermald.enable = true;
+ # services.tlp = {
+ #   enable = true;
+ #   settings = {
+ #     CPU_SCALING_GOVERNOR_ON_AC = "performance";
+ #     CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
+ #     CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
+ #     CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
+ #     CPU_MIN_PERF_ON_AC = 0; 
+ #     CPU_MAX_PERF_ON_AC = 100;
+ #     CPU_MIN_PERF_ON_BAT = 0;
+ #     CPU_MAX_PERF_ON_BAT = 20;
+ #
+ #   };
+ # };
 
   # Network
   networking = {
@@ -49,7 +50,7 @@
 
   # Xserver and graphics
   # Enable the X11 windowing system.
-  services.xserver.enable = true;
+#  services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
